da949d3448707ed6e8e8875f3982aa68165be52e
reymark <125524406+Rylllll@users.noreply.github.com>
2023-07-29 13:41:36 +0800

    dsa

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/**/*.{html,js}"],
+  content: ["./dist/**/*.{html,js}"],
   theme: {
     extend: {
       screens: {
